34fdfa54de4fcd3bdb7acaed842aa2e12ab801ec
jtCodes <jt@TRLDSC1.tch.harvard.edu>
2019-04-23 14:02:44 -0400

    added dist folder

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Folders = require("./Folders");
+
+var _Folders2 = _interopRequireDefault(_Folders);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Folders2.default;
