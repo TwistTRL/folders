6942ee1c885e1c90f686c444b1f18c6834532690
jtCodes <jt@TRLDSC1.tch.harvard.edu>
2019-04-23 14:48:47 -0400

    demo file setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Folders from "./lib";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const fluidsFolders = [
+            {
+                name: "COAG",
+                backgroundColor: "#fff5e8",
+                component:
+                    <>
+                        <div className="fluids-table-label-container">
+                            <h3>
+                                <span className="c-circle">C</span>
+                                <span className="coag-label">Coagulation</span>
+                            </h3>
+                        </div>
+                    </>,
+                panelLabel: "Nutrition"
+            },
+            {
+                name: "NUTRITION",
+                backgroundColor: "#b1cbf1",
+                component:
+                    <>
+                        <h3 className="nu-label">
+                            <span className="n-circle">N</span>
+                            <span className="coag-label">Nutrition</span>
+                        </h3>
+                    </>,
+                panelLabel: "NO"
+            },
+            {
+                name: "RENAL",
+                backgroundColor: "#e6bb14",
+                component: <h2> EMPTY </h2>,
+                panelLabel: "NO"
+            },
+        ]
+        return (
+            <Folders
+                width={this.props.width}
+                heightString={this.props.heightString}
+                className="fluids-folder"
+                folders={fluidsFolders} />
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
